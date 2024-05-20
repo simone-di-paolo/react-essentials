b89e284424b5d2517ae09f0167e569392befe7ab
s.dipaolo90 <s.dipaolo90@gmail.com>
2024-05-20 14:48:24 +0000

    Divided sections into different Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,17 @@
 import './App.css'
-import { CORE_CONCEPTS } from './data'
 import Header from './components/header/Header'
-import CoreConcept from './components/coreconcepts/CoreConcept'
-import TabButton from './components/TabButton'
-import { useState } from 'react'
-import { EXAMPLES } from './data'
+import CoreConcepts from './components/coreconcepts/CoreConcepts'
+import Examples from './components/examples/examples'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
- 
-  let tabComponent = <p>Please select a topic:</p>
-  if(selectedTopic) {
-    tabComponent = 
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-            {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
-      </div>
-  }
 
   return (
     <>
-      <div>
-        <Header />
-        <main>
-          <section id="core-concepts">
-            <h2>Core Concepts</h2>
-            <ul>
-              {CORE_CONCEPTS.map((conceptItem) => (
-                <CoreConcept key={conceptItem.title} {...conceptItem} />
-              ))}
-            </ul>
-          </section>
-          <section id="examples">
-            <h2>Examples</h2>
-            <menu>
-              <TabButton isSelected={selectedTopic==='components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-              <TabButton isSelected={selectedTopic==='jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-              <TabButton isSelected={selectedTopic==='props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-              <TabButton isSelected={selectedTopic==='state'} onSelect={() => handleSelect('state')}>State</TabButton>
-            </menu>
-            {tabComponent}
-          </section>
-        </main>
-      </div>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
     </>
   )
 }
