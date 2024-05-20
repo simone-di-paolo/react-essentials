900adb44bd8f27e0ff05fc898938e835e54a5078
s.dipaolo90 <s.dipaolo90@gmail.com>
2024-05-20 11:55:31 +0000

    Updated the App functionality of button clicks by showing the datas that comes from the data.js file

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import { CORE_CONCEPTS } from './data'
 import Header from './components/header/Header'
 import CoreConcept from './components/coreconcepts/CoreConcept'
 import TabButton from './components/TabButton'
+import { useState } from 'react'
+import { EXAMPLES }from './data'
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
 
   return (
     <>
@@ -23,11 +30,20 @@ function App() {
           <section id="examples">
             <h2>Examples</h2>
             <menu>
-              <TabButton>Components</TabButton>
-              <TabButton>JSX</TabButton>
-              <TabButton>Props</TabButton>
-              <TabButton>State</TabButton>
+              <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
             </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
           </section>
         </main>
       </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,13 +1,8 @@
-export default function TabButton({children}) {
-
-    function handleClick() {
-        console.log('clicked')
-    }
-
+export default function TabButton({children, onSelect}) {
     
     return (
     <li>
-        <button onClick={handleClick}>{children}</button>
+        <button onClick={onSelect}>{children}</button>
     </li>
     )
 }
